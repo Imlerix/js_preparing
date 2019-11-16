3de4d3e4a76f66ea474a927f95ca5f90f27782c9
Vadim Golenko <udachin.vadim@gmail.com>
2019-11-16 03:34:33 +0300

    add new questions and definitions

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+let list = {
+    value: 1,
+    next: {
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: null
+            }
+        }
+    }
+};
 
-console.log()
+function printList(list) {
+    list.next && printList(list.next)
+    console.log(list.value)
+}
 
+function printListCycle(list) {
+    let tmp = list
+    while (tmp) {
+        console.log(tmp.value)
+        tmp = tmp.next
+    }
+}
+
+// printList(list)
+printListCycle(list)
